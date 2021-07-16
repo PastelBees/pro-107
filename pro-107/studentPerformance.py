e9c25f7a742043d4e2e8f01cdf70042e214ca28a
PastelBees <73673440+PastelBees@users.noreply.github.com>
2021-07-16 16:42:23 -0400

    Update studentPerformance.py

--- pro-107/studentPerformance.py
+++ pro-107/studentPerformance.py
@@ -6,6 +6,6 @@ df = pd.read_csv("data.csv")
 
 mean = df.groupby(["student_id", "level"], as_index = False).mean()
 
-fig = px.scatter(mean, x="student_id", y="level", color="attempt", color="attempt")
+fig = px.scatter(mean, x="student_id", y="level", color="attempt", size="attempt")
 
 fig.show()
